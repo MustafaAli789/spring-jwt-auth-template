383db189c3f04311a50f4e50979ef46c9f410dac
MustafaAli789 <alimustafa0987@gmail.com>
2021-08-29 16:38:08 -0400

    initial commit

---
+++ src/main/kotlin/com/jwtAuth/JWTAuthTemplate/JwtAuthTemplateApplication.kt
@@ -0,0 +1,11 @@
+package com.jwtAuth.JWTAuthTemplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JwtAuthTemplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<JwtAuthTemplateApplication>(*args)
+}
---
+++ src/test/kotlin/com/jwtAuth/JWTAuthTemplate/JwtAuthTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jwtAuth.JWTAuthTemplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JwtAuthTemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

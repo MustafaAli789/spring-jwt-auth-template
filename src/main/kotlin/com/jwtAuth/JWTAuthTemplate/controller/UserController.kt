1a4f86c7279bfdf129fd6c999e97ec2cf81cee46
MustafaAli789 <alimustafa0987@gmail.com>
2021-08-30 15:00:59 -0400

    controller update

--- src/main/kotlin/com/jwtAuth/JWTAuthTemplate/controller/UserController.kt
+++ src/main/kotlin/com/jwtAuth/JWTAuthTemplate/controller/UserController.kt
@@ -1,12 +1,13 @@
 package com.jwtAuth.JWTAuthTemplate.controller
 
+import com.jwtAuth.JWTAuthTemplate.domain.Role
 import com.jwtAuth.JWTAuthTemplate.domain.User
 import com.jwtAuth.JWTAuthTemplate.service.UserService
 import lombok.RequiredArgsConstructor
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import java.net.URI
 
 @RestController()
 @RequestMapping("/api")
@@ -18,4 +19,23 @@ class UserController(val userService: UserService) {
         return ResponseEntity.ok().body(userService.getUsers())
     }
 
+    @PostMapping("/user/save")
+    fun saveUser(@RequestBody user: User): ResponseEntity<User> {
+        val uri: URI = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString())
+        return ResponseEntity.created(uri).body(userService.saveUser(user))
+    }
+
+    @PostMapping("/role/save")
+    fun saveRole(@RequestBody role: Role): ResponseEntity<Role> {
+        val uri: URI = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString())
+        return ResponseEntity.created(uri).body(userService.saveRole(role))
+    }
+
+    @PostMapping("/role/addtouser")
+    fun addRoleToUser(@RequestBody roleToUserForm: RoleToUserForm): ResponseEntity<Any> {
+        userService.addRoleToUser(roleToUserForm.username, roleToUserForm.roleName)
+        return ResponseEntity.ok().build()
+    }
 }
+
+data class RoleToUserForm(val username: String, val roleName: String)
